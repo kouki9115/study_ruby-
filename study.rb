8744a7f3275442f17b5f5c9eabdb448658bcf5ee
kouki9115 <kouki.n9115@gmail.com>
2021-06-23 06:33:27 +0000

    add study

---
+++ study.rb
@@ -0,0 +1,19 @@
+str = "Ruby"
+
+if str == "Ruby"
+    puts "Rubyと等しい"
+else
+  puts "等しくありません"
+end
+
+
+puts "50 or 100"
+input = gets.to_i
+
+if input == 50
+  puts "a"
+elsif input == 100
+  puts "b"
+else
+  puts input
+end
