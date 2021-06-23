e49c4ee4e164858e534334e2c0f7049c03344ae3
kouki9115 <kouki.n9115@gmail.com>
2021-06-23 06:57:39 +0000

    add gets

---
+++ gets.rb
@@ -0,0 +1,24 @@
+puts "2つの数字を入力して下さい"
+a = gets.to_i
+b = gets.to_i
+
+puts "答えは"
+puts a * b
+
+puts "計算回数を入力して下さい"
+input = gets.to_i
+i = 1
+while i <= input do
+
+puts "好きな数字を2つ入力して下さい"
+a = gets.to_i
+b = gets.to_i
+
+puts a + b
+puts a - b
+puts a * b
+puts a / b
+
+i = i+1
+
+end
