03436c6159ce689754309d628eeaa12160aa1408
kouki9115 <kouki.n9115@gmail.com>
2021-06-24 05:00:38 +0000

    update multipry

--- multipry.rb
+++ multipry.rb
@@ -1,9 +1,13 @@
-s = 1
-i = 1
+ def multi
+  s = 1
+  i = 1
 
-while i <= 10 do
-s = s * i
-i = i + 1
-end
+  while i <= 10 do
+  s = s * i
+  i = i + 1
+  end
+  puts s
 
-puts s
+ end
+
+puts multi
