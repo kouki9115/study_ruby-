6179bc6214e2325fb401042fcf1eb12b8531b308
kouki9115 <kouki.n9115@gmail.com>
2021-07-01 05:45:07 +0000

    add ruby practice

---
+++ ruby2.rb
@@ -0,0 +1,41 @@
+a = 20
+b = "xyz"
+c = [5,7,8]
+i = 1
+
+while i <= 5 do
+ if a == b
+  puts "a+b"
+ elsif i == 3
+  puts c[1]
+ else
+  puts "#{a}+#{b}+#{c}"
+ end
+ i = i + 1
+end
+
+c.each do |f|
+puts f
+ if f == 8
+  f = "zzz"
+ puts f
+ end
+end
+
+q = 1
+for q in 1..5
+ puts a
+q = q + 1
+a = a + q * 20
+
+  if a % 120 == 0
+   puts "finish"
+   break
+  end
+
+end
+
+
+
+
+
