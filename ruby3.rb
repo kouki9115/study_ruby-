46d739d1f62f83996e1914469162f6220a724f2d
kouki9115 <kouki.n9115@gmail.com>
2021-07-05 08:51:45 +0000

    ruby3

--- ruby3.rb
+++ ruby3.rb
@@ -3,23 +3,26 @@ puts "何回挑戦しますか？"
 count = gets.to_i
 puts "半ならば1or丁ならば2を入力して下さい"
 hit = gets.to_i
+puts "掛け金を入力して下さい"
+money = gets.to_i
 
 i = 1
 aaa = 0
 bbb = 0
+ccc = 0
 
 while i <= count do
 
 random = rand (1..6)
 
- if random % 2 == 0
+ if hit == 1 && random % 2 == 0
   puts "#{random}の半"
-  zzz = 1
   aaa = aaa + 1
- else
+ elsif hit == 2 and random == 5 || random == 3 || random == 1
    puts "#{random}の丁"
-   xxx = 2
   bbb = bbb + 1
+ else
+  ccc = ccc + 1
  end
  i = i + 1
 
@@ -28,11 +31,16 @@ end
  puts "半は#{aaa}回"
  puts "丁は#{bbb}回"
 
- if hit == zzz
+ if hit == 1
   puts "#{aaa}回　正解"
- elsif hit == xxx
+ elsif hit == 2
   puts "#{bbb}回 正解"
- else
-  puts "不正解"
-
  end
+  puts "#{ccc}回 不正解"
+
+money1 = money * aaa
+money2 = money * bbb
+money3 = money * ccc
+
+summoney = money1 + money2 - money3
+puts summoney
