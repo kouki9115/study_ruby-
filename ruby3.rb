6494966525618147923f4b1e54cc7c10883f6fe3
kouki9115 <kouki.n9115@gmail.com>
2021-07-05 07:38:47 +0000

    ruby3

--- ruby3.rb
+++ ruby3.rb
@@ -1,7 +1,10 @@
+puts "半丁ゲーム"
 puts "何回挑戦しますか？"
 count = gets.to_i
 
 i = 1
+aaa = 0
+bbb = 0
 
 while i <= count do
 
@@ -9,9 +12,14 @@ random = rand (1..6)
 
  if random % 2 == 0
   puts "#{random}の半"
+  aaa = aaa + 1
  else
    puts "#{random}の丁"
+  bbb = bbb + 1
  end
-
  i = i + 1
+
 end
+
+ puts "半は#{aaa}回"
+ puts "丁は#{bbb}回"
