b00412bb3f3123237caa8647b7008d2e8137e918
kouki9115 <kouki.n9115@gmail.com>
2021-07-12 06:18:33 +0000

    update FizzBuzz

--- practice3.rb
+++ practice3.rb
@@ -1,3 +1,23 @@
+puts "入力して下さい"
+input = gets.to_i
+
+def aaa(num)
+
+if num % 15 == 0
+  puts "FizzBuzz"
+elsif num % 5 == 0
+  puts "Fizz"
+elsif num % 3 == 0
+  puts "Buzz"
+else
+  puts num
+end
+
+end
+puts "結果は"
+puts aaa(input)
+
+
 puts "数字を入力して下さい"
 i = gets.to_i
 
