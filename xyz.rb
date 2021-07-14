5225314baf2f4d1ab389e00fb23b6579d5be4fc5
kouki9115 <kouki.n9115@gmail.com>
2021-07-14 07:01:49 +0000

    update xyz

--- xyz.rb
+++ xyz.rb
@@ -1,17 +1,18 @@
 x = 1
-y = 2
-z = 3
+y = 3
+z = 6
 
 for i in 1..5 do
 
-if x <= y
+if x < y
  puts "#{y}は#{x}より大きい"
-elsif y <= z
+elsif y < z
  puts "#{z}は#{y}より大きい"
 else
- puts xyz
+ puts "finish"
 end
 
-x = x + 1
-
+x = x + 2
+y = y + 3
+z = z + 2
 end
