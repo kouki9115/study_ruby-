624cd632dc059cbe842da148da7a4fd44399fa17
kouki9115 <kouki.n9115@gmail.com>
2021-07-17 04:39:35 +0000

    add FizzBuzz

---
+++ FizzBuzz3.rb
@@ -0,0 +1,15 @@
+input = gets.to_i
+
+def FizzBuzz(n)
+ if n % 15 == 0
+  puts "FizzBuzz"
+ elsif n % 15 == 0
+  puts "FizzBuzz"
+ elsif n % 15 == 0
+  puts "FizzBuzz"
+ else
+  puts n
+ end
+end
+
+puts FizzBuzz(input)
