85f8f6c50600a5e80d73730ee3a3a087aa37ba54
kouki9115 <kouki.n9115@gmail.com>
2021-07-23 01:12:52 +0000

    update def2.rb

--- def2.rb
+++ def2.rb
@@ -4,8 +4,16 @@ def aaa(a)
   puts "finish"
  else
   puts "replay"
+  a = rand(1..3)
+  puts a
+   if a == 1
+    puts "finish"
+   else
+    puts "replay"
+   end
  end
 end
 
 random = rand(1..5)
+puts random
 puts aaa(random)
