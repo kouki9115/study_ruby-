91de31ab7ab6f1933100678fe6d23f2db277e659
kouki9115 <kouki.n9115@gmail.com>
2021-07-23 01:18:10 +0000

    update def2.rb

--- def2.rb
+++ def2.rb
@@ -1,26 +1,26 @@
 def aaa(a)
 
  if a == 1
-  puts "finish"
+  puts "10分の1を引き当てて,finish"
  else
   puts "replay"
-  a = rand(1..3)
+  a = rand(1..5)
   puts a
    if a == 1
-    puts "finish"
+    puts "5分の1を引き当てて,finish"
    else
     puts "replay"
     a = rand(1..2)
     puts a
     if a == 1
-     puts "finish"
+     puts "2分の1を引き当てて,finish"
     else
-     puts "replay"
+     puts "2分の1すらも外す人間"
     end
    end
  end
 end
 
-random = rand(1..5)
+random = rand(1..10)
 puts random
 puts aaa(random)
