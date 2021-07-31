09ab08acc7a69c14929298d69aa8ecef2990c142
kouki9115 <kouki.n9115@gmail.com>
2021-07-31 04:23:34 +0000

    update xyz2.rb

--- xyz2.rb
+++ xyz2.rb
@@ -1,12 +1,16 @@
 
 
 def xyz
-a = rand(1..2)
-puts a
+ i = 1
+ while i <= 2 do
+  a = rand(1..2)
+  puts a
  if a % 2 == 0
   puts "#{a}は偶数である"
  else
   puts "#{a}は奇数である"
+ end
+ i = i + 1
  end
 end
 
