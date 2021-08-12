428897c265593b923ce9ed029978cd5ce89b7efc
kouki9115 <kouki.n9115@gmail.com>
2021-08-12 01:39:11 +0000

    update xyz3

--- xyz3.rb
+++ xyz3.rb
@@ -1,12 +1,19 @@
+i = 1
 
-a = rand(1..6)
-b = rand(1..3)
-c = rand(1..3)
+while i <= 10 do
 
-if a == b
-  puts "zzz"
-elsif b == c
- puts "xyz"
-else
- puts "exit"
+ a = rand(1..6)
+ b = rand(1..3)
+ c = rand(1..3)
+
+ if a == b
+   puts "zzz"
+ elsif b == c
+  puts "xyz"
+ else
+  puts "exit"
+ end
+
+ i = i + 1
+
 end
